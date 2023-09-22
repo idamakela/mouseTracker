2aa393ebec674a2a7b8e323274d03a8de25864a1
idamakela <idamakela98@gmail.com>
2023-09-22 15:18:54 +0200

    funny object for things to be displayed in the ui thihi

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,48 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import MouseTracker from './components/MouseTracker';
 
+/*
+ * when an element is clicked, update the UI from what element is clicked
+ * if its clicked twice, make something funny 
+ */
+
+// interface HTMLObject {
+//     element: string[]: {
+//         firstClick: string,
+//         secondClick: string,
+//         thirdClick: string,
+//     }
+// }
+
+const HtmlElements: HTMLObject = {
+    header:  [
+        "header",
+        "header, again",
+        "don't you know this is the header already?"
+    ],
+    main: [
+        "main",
+        "main again",
+        "THIS IS THE MAIN ELEMENT!"
+    ],
+    textContainer: [
+        "text container",
+        "TEXT container",
+        "an area where text is contained..."
+    ],
+    footer: [
+        "footer",
+        "the foot of the website",
+        "the bottom part of the website, aka footer."
+    ],
+    ida: [
+        "that's my name",
+        "my last-name is Finish",
+        "i have a github you know, you could just go there to find out about me: @idamakela"
+    ]
+
+}
+
 function App() {
   const [changeCursor, setChangeCursor] = useState<boolean>(false);
 
